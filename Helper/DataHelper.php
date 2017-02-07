58d6fa4d1daf84d08b7dcdc1d12d0f3a0a473e92
System Administrator <root@USBUR151004322L.local>
2017-02-06 17:22:01 -0800

    cleanup

--- Helper/DataHelper.php
+++ Helper/DataHelper.php
@@ -4,19 +4,6 @@ namespace JoshSpivey\LiteView\Helper;
 class DataHelper extends \Magento\Framework\App\Helper\AbstractHelper
 {
 
-	public function convert_array_to_xml($array, $namespace = '') {
-		$xml = '';
-		foreach($array as $key => $value) {
-			if(is_array($value)) {
-				$value = $this->convert_array_to_xml($value);
-			}
-			$xml .= "<$key" . rtrim(" $namespace") . ">$value</$key>";
-		}
-		
-		return $xml;
-	}
-
-
 	public function array_to_xml($data, &$xml) {
 	    foreach($data as $key => $value) {
 	        if(is_array($value)) {
